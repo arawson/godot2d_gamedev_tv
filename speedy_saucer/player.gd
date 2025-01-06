54bcc5fe408367e8e1bc0288a2d9018171683281
Aaron Rawson <aaronmrawson@gmail.com>
2025-01-06 07:53:25 -0500

    further lesson items

---
+++ speedy_saucer/player.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	test()
+
+
+func test():
+	print("This is a test of the emergency broadcast system." + " this is only a test")
